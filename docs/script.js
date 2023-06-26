5d97e61664e8e62fccf7e2244b47ecb12e8ba581
ivancamilo10 <ivantriana2005@gmail.com>
2023-06-25 20:26:20 -0500

    hice estructura y diseño
    aclaro aun faltan muchos detalles
    
    #ivan tu puedes!

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+function enviarFormulario(event) {
+    event.preventDefault(); // Evita que el formulario se envíe y se recargue la página
+    
+    // Obtén los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var fecha = document.getElementById("fecha").value;
+    
+    // Realiza
+}
